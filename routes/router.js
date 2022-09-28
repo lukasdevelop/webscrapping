c3a5ceb38e656e0a4533877b317356f6caaa4b57
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2022-09-28 15:03:31 -0300

    Configurações iniciais

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const BrowserService = require('../services/BrowserService');
+
+const se = new BrowserService()
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Hello Asksuite World!');
 });
 
-//TODO implement endpoint here
+router.post('/search', async (req, res) => {
+
+    const { checkin, checkout } = req.body
+
+    await BrowserService.getBrowser()
+
+    res.send(
+        'teste'
+    )
+})
+
 
 module.exports = router;
--- services/BrowserService.js
+++ services/BrowserService.js
@@ -2,8 +2,14 @@ const puppeteer = require('puppeteer');
 
 class BrowserService {
 
-    static getBrowser() {
-        return puppeteer.launch({});
+     browser;
+     page;
+
+    static async getBrowser() {
+        this.browser = await puppeteer.launch({ headless: false});
+        this.page = await this.browser.newPage();
+        await this.page.goto(`https://pratagy.letsbook.com.br/D/Reserva?checkin=01%2F10%2F2022&checkout=04%2F10%2F2022&cidade=&hotel=12&adultos=1&criancas=&destino=Pratagy+Beach+Resort+All+Inclusive&promocode=&tarifa=&mesCalendario=9%2F1%2F2022`)
+        
     }
 
     static closeBrowser(browser) {
